a5d23b188701efb5a700d133eb0d55305e47750f
jeanette.munoz <jeanette.munoz@adalove.com.ar>
2024-05-15 17:45:55 -0300

    Resolucion del desafio

--- modulo1/08 Clase - High Order Functions/Desafio/index.js
+++ modulo1/08 Clase - High Order Functions/Desafio/index.js
@@ -4,22 +4,24 @@ const app = express();
 const PORT = 3000;
 
 function getAllProductos() {
-    return productos ? productos :
+    return productos ? productos.sort((a, b) => a.nombre.localeCompare(b.nombre)) :
         { id: 'Error', descripcion: 'No se encontraron coincidencias.' }
 }
 
 app.get('/productos', (req, res) => { 
-    res.json(getAllProductos())
+    res.json(getAllProductos());
 })
 
-app.get('/productos/:id', (req, res) => {
-
-})
- 
-app.get('/productos/:nombre', (req, res) => { 
-
-})
-
+// Ruta para obtener un producto por ID
+app.get('/productos/id/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const producto = productos.find(p => p.id === id);
+    if (producto) {
+        res.json(producto);
+    } else {
+        res.status(404).json({ error: 'Producto no encontrado' });
+    }
+});
 
 app.get('*', (req, res) => {
     res.status(403).send("Lo siento no es una ruta de mi proyecto! Ruta con get * ")
