b421b0345c3f8f3528e989c837fa4bde94a3f1f9
jeanette.munoz <jeanette.munoz@adalove.com.ar>
2024-06-03 18:08:28 -0300

    resolucion del ejercicio de la clase 13

--- modulo2/Clase13 - MongoDB + Nodejs/mongoDb+Nodejs/server.js
+++ modulo2/Clase13 - MongoDB + Nodejs/mongoDb+Nodejs/server.js
@@ -41,6 +41,35 @@ app.get('/frutas/:id', async (req, res) => {
     !fruta ? res.status(404).send('No encontre la fruta con el id '+ frutaID): res.status(200).json(fruta)
 });
 
+app.get('/frutas/nombre/:nombre', async (req, res) => {
+    const nombreFruta = req.params.nombre
+    const client = await connectToMongodb();
+    if (!client) {
+        res.status(500).send('Error al conectarse a MongoDB')
+        return;
+    }
+    const regex = new RegExp(nombreFruta.toLowerCase(), 'i');
+    const db = client.db('frutas')
+    const frutas = await db.collection('frutas').find({ nombre: regex}).toArray()
+    await disconnectToMongodb()
+    frutas.length == 0 ? res.status(404).send('No encontre la fruta con el nombre '+ nombreFruta): res.json(frutas)
+})
+
+app.get('/frutas/precio/:precio', async (req, res) => {
+    const precioFruta = parseInt(req.params.precio) || 0
+    const client = await connectToMongodb();
+    if (!client) {
+        res.status(500).send('Error al conectarse a MongoDB')
+        return;
+    }
+    const db = client.db('frutas') 
+    // gte: mayor o igual a
+    const frutas = await db.collection('frutas').find({ importe: { $gte: precioFruta } }).toArray()
+    await disconnectToMongodb()
+    frutas.length == 0 ? res.status(404).send('No encontre la fruta con el precio '+ precioFruta): res.json(frutas)
+
+})
+
 app.get("*", (req, res) => {
     res.json({
       error: "404",
