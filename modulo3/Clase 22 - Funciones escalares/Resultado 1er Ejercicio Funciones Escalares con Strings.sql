363c3155b9431e7f445abfe1e493e56e6a386e6e
jeanette.munoz <jeanette.munoz@adalove.com.ar>
2024-07-03 19:21:45 -0300

    CLASE 22

--- modulo3/Clase 22 - Funciones escalares/Resultado 1er Ejercicio Funciones Escalares con Strings.sql
+++ modulo3/Clase 22 - Funciones escalares/Resultado 1er Ejercicio Funciones Escalares con Strings.sql
@@ -1,17 +1,25 @@
--- 1 Selección de los campos de la tabla productos, aplicando alias en los nombres
-SELECT ProductID AS Codigo, ProductName AS Descripcion, QuantityPerUnit AS Presentacion, UnitPrice AS PrecioUnitario
-FROM products;
 
--- 2 Selección de los campos de la tabla productos, definiendo el nombre en Mayúsculas
-SELECT ProductID, UCASE(ProductName) AS ProductName, QuantityPerUnit, UnitPrice
-FROM products;
+/* 1. Ejecuta una consulta de selección sobre los siguientes campos de la tabla Products
+○ productID, productName, QuantityPerUnit, UnitPrice
+○ aplica un alias a cada uno de ellos (Codigo, Descripcion, Presentacion, PrecioUnitario)*/
+SELECT productID AS 'Codigo', productName AS 'Descripcion', QuantityPerUnit AS Presentacion, UnitPrice AS 'Precio Unitario' FROM northwind.products;
 
--- 3 Selección de los campos ProductID, ProductName, QuantityPerUnit de la tabla productos,
---   reemplazando en el último la palabra 'boxes' por 'cajas'
-SELECT 
-    ProductID AS Codigo, 
-    UCASE(ProductName) AS Descripcion, 
-    REPLACE(QuantityPerUnit, 'boxes', 'cajas') AS Presentacion, 
-    UnitPrice AS PrecioUnitario
+/*2. Ejecuta una consulta de selección sobre los siguientes campos de la tabla Products
+○ ProductID, ProductName, QuantityPerUnit, UnitPrice
+○ Aplica sobre el campo ProductName la función escalar que transforma el texto a mayúsculas*/
+SELECT ProductID AS 'Codigo', UCASE(ProductName) AS Descripcion, QuantityPerUnit AS Presentacion, UnitPrice  AS 'Precio Unitario'
+FROM northwind.Products;
+
+/*3. Ejecuta una consulta de selección sobre los siguientes campos de la tabla Products
+○ ProductID, ProductName, QuantityPerUnit, UnitPrice
+○ aplica el mismo alias detallado en el punto uno (1)
+○ Aplica sobre el campo QuantityPerUnit la función escalar de reemplazo de texto, buscando
+el texto ‘boxes’ y reemplazando el mismo por ‘cajas’
+○ la condición WHERE debe filtrar aquellos registros que posean en cualquier parte la palabra ‘boxes’
+en cualquier parte del campo QuantityPerUni*/
+SELECT productID AS Codigo, 
+productName AS Descripcion, 
+REPLACE(QuantityPerUnit, 'boxes', 'cajas') AS Presentacion, 
+UnitPrice AS PrecioUnitario
 FROM products
 WHERE QuantityPerUnit LIKE '%boxes%';
--- modulo3/Clase 22 - Funciones escalares/Resultado 2do Ejercicio con Funciones Escalares con Strings y Date.sql
+++ modulo3/Clase 22 - Funciones escalares/Resultado 2do Ejercicio con Funciones Escalares con Strings y Date.sql
@@ -1,15 +1,22 @@
--- Consulta de selección base
+/*  Necesitamos simplificar la visualización de datos de esta tabla, presentando en una consulta de 
+selección, los siguientes campos:
+● EmployeeID, TitleOfCourtesy, LastName, FirstName, Title, BirthDate, HireDate
+Sobre esta consulta de selección base, realiza las siguientes consignas:*/
 SELECT EmployeeID, TitleOfCourtesy, LastName, FirstName, Title, BirthDate, HireDate
 FROM Employees;
 
--- 1
+/* 1. En una nueva consulta de selección con la base anterior, concatena los campos:
+a. (TitleOfCourtesy, LastName, FirstName) con el alias NombreCompleto
+b. respeta los espacios entre los diferentes campos mencionados*/
 SELECT EmployeeID, concat(TitleOfCourtesy, ' ', LastName, ' ', FirstName) AS NombreCompleto,
 	   Title, 
        BirthDate, 
        HireDate
 FROM Employees;
 
--- 2
+/* 2. En una nueva consulta de selección con la base inicial:
+a. elimina el formato fecha y hora sobre el campo BirthDate, utilizando la función Date()
+b. aplica un alias a dicho campo para llamarlo FechaNacimiento*/
 SELECT EmployeeID, 
 	   TitleOfCourtesy, 
        LastName, 
@@ -19,7 +26,9 @@ SELECT EmployeeID,
        HireDate
 FROM Employees;
 
--- 3
+/* 3. Copia la consulta resultante del punto dos, y modifícala aplicando lo siguiente:
+a. utiliza la función YEAR sobre campo HireDate, para mostrar sólo el año de contratación
+b. aplica un alias a dicha campo, para llamarlo AnioContratacio */
 SELECT EmployeeID, 
 	   TitleOfCourtesy, 
        LastName, 
